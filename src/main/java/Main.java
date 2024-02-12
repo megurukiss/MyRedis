97c5df1a16c970c72a6be3a770703a7adcf41fdd
meguru_kiss <chf007@ucsd.edu>
2024-02-11 22:20:45 -0800

    pass 3rd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,33 +9,43 @@ public class Main {
 
     //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
-        Socket clientSocket = null;
         int port = 6379;
         try {
           serverSocket = new ServerSocket(port);
           serverSocket.setReuseAddress(true);
             while(true){
-                clientSocket = serverSocket.accept();
-                BufferedReader reader=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter writer=new PrintWriter(clientSocket.getOutputStream(), true);
-                String line;
-                while((line=reader.readLine())!=null){
-                    if (line.equalsIgnoreCase("PING")) {
-                        writer.print("+PONG\r\n");
-                        writer.flush();
-                    }
-                }
+                Socket clientSocket = serverSocket.accept();
+                new Thread(() ->{
+                    handleClient(clientSocket);
+                }).start();
             }
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
         }
   }
+
+  public static void handleClient(Socket clientSocket) {
+      try {
+          BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+          PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+          String line;
+          while ((line = reader.readLine()) != null) {
+              if (line.equalsIgnoreCase("PING")) {
+                  writer.print("+PONG\r\n");
+                  writer.flush();
+              }
+          }
+      } catch (IOException e) {
+          System.out.println("IOException: " + e.getMessage());
+      }finally {
+          try {
+              if (clientSocket != null) {
+                  clientSocket.close();
+              }
+          } catch (IOException e) {
+              System.out.println("IOException: " + e.getMessage());
+          }
+      }
+  }
+
 }
