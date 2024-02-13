18151ed79e549ac2fadeb1bf5795e4bc6476a1ad
meguru_kiss <chf007@ucsd.edu>
2024-02-12 19:45:13 -0800

    pass 9rd stage

--- src/main/java/MasterServer.java
+++ src/main/java/MasterServer.java
@@ -1,4 +1,5 @@
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class MasterServer extends RedisServer{
     String role = "master";
@@ -8,6 +9,55 @@ public class MasterServer extends RedisServer{
         super();
     }
 
+    @Override
+    public void handleCommand(ArrayList<String> commandArray, PrintWriter writer){
+        int commandLength = Integer.parseInt(commandArray.getFirst().substring(1));
+        String command = commandArray.get(2);
+        switch (command.toLowerCase()){
+            case "ping":
+                handlePing(writer);
+                break;
+            case "echo":
+                handleEcho(commandArray.get(4), writer);
+                break;
+            case "replconf":
+                handleReplconf(writer);
+                break;
+            case "set":
+                if(commandLength==3) {
+                    handleSet(commandArray.get(4), commandArray.get(6), writer);
+                }else if(commandLength==5){
+                    if(commandArray.get(8).equalsIgnoreCase("px")){
+                        handleSetWithExpiry(commandArray.get(4), commandArray.get(6), commandArray.get(10), writer);
+                    }
+                }else{
+                    writer.print("-ERR syntax error\r\n");
+                    writer.flush();
+                }
+                break;
+            case "get":
+                handleGet(commandArray.get(4), writer);
+                break;
+            case "info":
+                if(commandArray.get(4).equalsIgnoreCase("replication")){
+                    handleInfo(writer);
+                }
+                else{
+                    writer.print("-ERR unknown subcommand or wrong number of arguments for 'info'\r\n");
+                    writer.flush();
+                }
+                break;
+            default:
+                writer.print("-ERR unknown command '"+command+"'\r\n");
+                writer.flush();
+                break;
+        }
+    }
+
+    public void handleReplconf(PrintWriter writer){
+        writer.print("+OK\r\n");
+        writer.flush();
+    }
     @Override
     public void handleInfo(PrintWriter writer){
         StringBuilder response = new StringBuilder();
