639f00223e17e611177963eaf64486b64748180b
meguru_kiss <chf007@ucsd.edu>
2024-02-14 15:54:50 -0800

    slave server read message, read file implemented

--- src/main/java/SlaveServer.java
+++ src/main/java/SlaveServer.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class SlaveServer extends RedisServer{
     String role = "slave";
@@ -157,19 +158,151 @@ public class SlaveServer extends RedisServer{
     }
     public void connectToMaster(){
         try{
-            masterSocket = new Socket(MasterIp, MasterPort);
-            PrintWriter writer = new PrintWriter(new OutputStreamWriter(masterSocket.getOutputStream()));
+            Socket tempSocket = new Socket(MasterIp, MasterPort);
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(tempSocket.getOutputStream()));
             String pingCommand= toRESP(new String[]{"PING"});
             String replconfigCommand1= toRESP(new String[]{"REPLCONF","listening-port",String.valueOf(MasterPort)});
             String replconfigCommand2= toRESP(new String[]{"REPLCONF","capa","psync2"});
             String psyncCommand= toRESP(new String[]{"PSYNC","?","-1"});
             writer.print(pingCommand);
+            // receive message like +PONG and check if it is +PONG
+           checkMessage(readMessage(tempSocket),"+PONG");
             writer.print(replconfigCommand1);
+            checkMessage(readMessage(tempSocket),"+OK");
             writer.print(replconfigCommand2);
+            checkMessage(readMessage(tempSocket),"+OK");
             writer.print(psyncCommand);
+            checkMessageByRegex(readMessage(tempSocket),"^\\+FULLRESYNC ([a-fA-F0-9]+) ([0-9]+)$");
+            readMessage(tempSocket);
+            byte[] fileContent= readRDBFile(tempSocket);
+
+            masterSocket = tempSocket;
             writer.flush();
         }catch (IOException e){
             e.printStackTrace();
         }
     }
+
+    public static String readMessage(Socket socket) throws IOException{
+        InputStream is = socket.getInputStream();
+        int ch;
+        StringBuilder sb = new StringBuilder();
+        while((ch = is.read()) != -1){
+            // skip starting \r\n
+            if(ch=='\r' || ch=='\n'){
+                continue;
+            }
+            if(ch=='+'){
+                sb.append((char)ch);
+                while((ch = is.read()) != -1){
+                    if(ch=='\r' || ch=='\n'){
+                        break;
+                    }
+                    sb.append((char)ch);
+                }
+                break;
+            }
+            else{
+                throw new IOException("Not a valid message");
+            }
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
+
+    public static void checkMessage(String message, String expected) throws IOException{
+        if(message.equals(expected)){
+            return;
+        }
+        throw new IOException("Connection to master failed");
+    }
+
+    public static void checkMessageByRegex(String message,String regex) throws IOException{
+        Pattern pattern = Pattern.compile(regex);
+        if(!pattern.matcher(message).matches()){
+            throw new IOException("Connection to master failed");
+        }
+    }
+
+    public static byte[] readRDBFile(Socket socket) throws IOException{
+        InputStream is = socket.getInputStream();
+        int ch;
+        while((ch=is.read())!=-1){
+            // skip heading \r\n
+            if(ch=='\r' || ch=='\n'){
+                continue;
+            }
+            if(ch=='$'){
+                // read array length until \r\n
+                StringBuilder sb= new StringBuilder();
+                while((ch=is.read())!=-1){
+                    if(ch=='\r' || ch=='\n'){
+                        break;
+                    }
+                    sb.append((char)ch);
+                }
+                int arrayLength = Integer.parseInt(sb.toString());
+                // create byte array to store file content
+                byte[] fileContent = new byte[arrayLength];
+                // read file content
+                int label= is.read(fileContent,0,arrayLength);
+                if(label==-1){
+                    throw new IOException("Error reading file content");
+                }
+                return fileContent;
+            }
+            else{
+                throw new IOException("Not a valid message");
+            }
+        }
+        throw new IOException("Error reading file content");
+    }
+
+    public ArrayList<String> readCommand(Socket socket) throws IOException{
+        InputStream is = socket.getInputStream();
+        int ch;
+        while((ch = is.read()) != -1){
+            // skip starting \r\n
+            if(ch=='\r' || ch=='\n'){
+                continue;
+            }
+            if(ch=='*'){
+                // read array length until \r\n
+                StringBuilder lengthString= new StringBuilder();
+                while((ch=is.read())!=-1){
+                    if(ch=='\r' || ch=='\n'){
+                        break;
+                    }
+                    lengthString.append((char)ch);
+                }
+                int halfLength = Integer.parseInt(lengthString.toString());
+                // convert ascii to integer
+                int arrayLength = halfLength*2;
+                // initialize array to store command
+                ArrayList<String> commandArray = new ArrayList<>();
+                commandArray.add("*"+halfLength);
+                // skip \r\n
+                is.read();
+                is.read();
+                // read array elements
+                while(arrayLength>0){
+                    StringBuilder sb = new StringBuilder();
+                    while((ch = is.read()) != -1){
+                        if(ch=='\r'){
+                            break;
+                        }
+                        sb.append((char)ch);
+                    }
+                    is.read();
+                    commandArray.add(sb.toString());
+                    arrayLength--;
+                }
+                return commandArray;
+            }else{
+                throw new IOException("Not a valid command");
+            }
+        }
+        throw new IOException("Error reading command");
+    }
+
 }
